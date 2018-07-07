481a795ac0da75eea5d7c4793ec2fc565f7294bf
Erik Ferragut <erikferragut@gmail.com>
2018-07-07 14:55:13 -0400

    moved some things around so it runs not on cloud9

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 from app import views
 
  
---
+++ app/views.py
@@ -0,0 +1,48 @@
+import os
+from app import app
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [Required()])
+    submit = SubmitField('Submit')
+    
+
+# abspath = (os.path.abspath(os.curdir))
+key = open('app/apiKey').readline()
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+    
+@app.route('/home')
+def home():
+    return render_template('home.html', my_string="nathan")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/project', methods=['GET','POST'])
+def project():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        print('name', name, form.validate_on_submit())
+    return render_template('project.html', form=form, name=name)
+
+    
+@app.route('/a')
+def a():
+    return render_template('infoWindow.html', key=key, 
+    markers = [{'latitude':30, 'longitude':130, 'markerContent':'This is Uluru'},
+    {'latitude':36, 'longitude':136, 'markerContent':'This is Osaka'}])
